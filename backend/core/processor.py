57f62e5b603b28fd8abd56b087e90efef254fc48
pxxguin <janghongseo@naver.com>
2026-04-28 20:27:32 +0900

    fix: update VideoWriter codec from avc1 to mp4v for improved compatibility

--- backend/core/processor.py
+++ backend/core/processor.py
@@ -71,7 +71,7 @@ class MotionTracker(BaseVisionProcessor):
         height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
         fps    = cap.get(cv2.CAP_PROP_FPS)
         
-        fourcc = cv2.VideoWriter_fourcc(*'avc1')
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
         out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
         
         backSub = cv2.createBackgroundSubtractorMOG2(
@@ -129,7 +129,7 @@ class MotionTracker(BaseVisionProcessor):
         height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
         fps = cap.get(cv2.CAP_PROP_FPS)
         
-        fourcc = cv2.VideoWriter_fourcc(*'avc1')
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
         out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
         
         p1 = (int(bbox[0]), int(bbox[1]))
@@ -171,7 +171,7 @@ class MotionTracker(BaseVisionProcessor):
         height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
         fps    = cap.get(cv2.CAP_PROP_FPS)
         
-        fourcc = cv2.VideoWriter_fourcc(*'avc1')
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
         out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
         
         while True:
